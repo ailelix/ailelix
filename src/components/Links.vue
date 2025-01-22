1b61989cf8b81eb5b275a634331d3cd5f9808dfb
Ailelix <felixchen531@gmail.com>
2025-01-22 10:06:16 +0000

    Update Links & Contact

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -5,7 +5,7 @@
       一些零碎的记录，只为了我自己的记录<br>
       Quartz + Obsidian
     </p><hr>
-    <a href="">Ailelix's Service Monitor</a>
+    <a href="https://mon.ailelix.com">Ailelix's Service Monitor</a>
     <p>
       服务状态监控<br>
       Powered by UptimeKuma
